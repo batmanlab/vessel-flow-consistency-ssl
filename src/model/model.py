3dace749efaa42e1c37dfaa2aa664bca1a6b231d
rohitrango <rohit.rango@gmail.com>
2020-06-29 10:32:31 -0400

    Change config and delete bad file

--- src/model/model.py
+++ src/model/model.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 from base import BaseModel
 from model.fcn32s import *
 from model.resnet import *
+from model.deepnets import *
 
 class MnistModel(BaseModel):
     def __init__(self, num_classes=10):
